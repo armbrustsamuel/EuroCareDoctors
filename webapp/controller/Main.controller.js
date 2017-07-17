5e84ceaa81664a664855df18e0d8342c794588ca
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2017-07-17 16:16:08 -0300

    Initial structure of project

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("EuroCareDoctor.controller.Main", {
+		
+		onInit : function (evt) {
+			// set mock model
+			var sPath = jQuery.sap.getModulePath("com.sap.EuroCareDoctor","/model/tiles.json");
+			var oModel = new JSONModel(sPath);
+			this.getView().setModel(oModel);
+		},
+		
+		onChatBotClicked: function () {
+			sap.ui.getCore().byId("app").to("idChatBot");
+		}
+		
+	});
+});
