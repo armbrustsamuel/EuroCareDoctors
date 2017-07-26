f93c0fcc76b8c4bcc1667f40639ce1cf5e5baae0
Armbrust Samuel <samuel.armbrust@sap.com>
2017-07-26 03:30:41 +0000

    Patient overview page first idea

--- webapp/controller/PatientOverview.controller.js
+++ webapp/controller/PatientOverview.controller.js
@@ -1,44 +1,32 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("EuroCareDoctor.controller.PatientOverview", {
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf EuroCareDoctor.view.PatientOverview
-		 */
-		//	onInit: function() {
-		//
-		//	},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf EuroCareDoctor.view.PatientOverview
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf EuroCareDoctor.view.PatientOverview
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf EuroCareDoctor.view.PatientOverview
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		
+		oPatientList: new JSONModel(),
+		oFeedMessage: new JSONModel(),
+		
+		sDestinationUrl : "eurocarebackend",
+		
+		onInit: function () {
+			this.oPatientList.setData({ 
+	            patientList : []
+	        });
+	        
+	        this.oFeedMessage.setData({
+	        	feedList : []	
+	        });
+	        
+	        this.getView().setModel(this.oFeedMessage, "Feed");
+	        this.getView().setModel(this.oPatientList, "Patient");
+		},
+		
+		handleNavButtonPress: function () {
+			sap.ui.getCore().byId("app").back();
+		}
 
 	});
 
